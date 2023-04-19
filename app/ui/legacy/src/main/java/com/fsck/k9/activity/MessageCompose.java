6563c64ad9d01544720b13ac1587551fe477ea3b
rayhankinan <rayhankinan@gmail.com>
2023-04-19 19:02:25 +0700

    Add signing

--- app/ui/legacy/src/main/java/com/fsck/k9/activity/MessageCompose.java
+++ app/ui/legacy/src/main/java/com/fsck/k9/activity/MessageCompose.java
@@ -3,6 +3,11 @@ package com.fsck.k9.activity;
 
 import java.io.File;
 import java.io.IOException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -32,7 +37,6 @@ import androidx.appcompat.app.ActionBar;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.util.TypedValue;
 import android.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
@@ -87,6 +91,10 @@ import com.fsck.k9.controller.MessageReference;
 import com.fsck.k9.controller.MessagingController;
 import com.fsck.k9.controller.MessagingListener;
 import com.fsck.k9.controller.SimpleMessagingListener;
+import com.fsck.k9.ecdsa.DigitalSigning;
+import com.fsck.k9.ecdsa.EllipticalCurve;
+import com.fsck.k9.ecdsa.EllipticalCurveKey;
+import com.fsck.k9.ecdsa.Point;
 import com.fsck.k9.fragment.AttachmentDownloadDialogFragment;
 import com.fsck.k9.fragment.AttachmentDownloadDialogFragment.AttachmentDownloadCancelListener;
 import com.fsck.k9.fragment.ProgressDialogFragment;
@@ -262,7 +270,7 @@ public class MessageCompose extends K9Activity implements OnClickListener,
     private String encryptionKeyString = "";
     private CheckBox encryptionKeyCheckBox;
 
-    //TODO: add attribute for signature: Ini bisa ikutin App.java di repo Kinan
+    //TODO: add attribute for signature
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -360,7 +368,41 @@ public class MessageCompose extends K9Activity implements OnClickListener,
         messageContentView = findViewById(R.id.message_content);
         messageContentView.getInputExtras(true).putBoolean("allowEmoji", true);
 
-        // TODO: signature
+        // TODO: signature @rayhankinan
+
+        BigInteger a = new BigInteger("FFFFFFFF00000001000000000000000000000000FFFFFFFFFFFFFFFFFFFFFFFC", 16);
+        BigInteger b = new BigInteger("5AC635D8AA3A93E7B3EBBD55769886BC651D06B0CC53B0F63BCE3C3E27D2604B", 16);
+        BigInteger p = new BigInteger("FFFFFFFF00000001000000000000000000000000FFFFFFFFFFFFFFFFFFFFFFFF", 16);
+
+        EllipticalCurve ellipticalCurve = new EllipticalCurve(a, b, p);
+        Point basePoint = new Point(
+            new BigInteger("6B17D1F2E12C4247F8BCE6E563A440F277037D812DEB33A0F4A13945D898C296", 16),
+            new BigInteger("4FE342E2FE1A7F9B8EE7EB4A7C0F9E162BCE33576B315ECECBB6406837BF51F5", 16));
+        BigInteger n = new BigInteger("FFFFFFFF00000000FFFFFFFFFFFFFFFFBCE6FAADA7179E84F3B9CAC2FC632551", 16);
+
+        // TODO: INI DIGANTI DENGAN EMAIL
+        byte[] message = "Hello World!".getBytes();
+
+        // TODO: INI DIGANTI DENGAN ALGORITMA GAGAS
+        MessageDigest messageDigest;
+        try {
+            messageDigest = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+        byte[] hash = messageDigest.digest(message);
+
+        try {
+            PrivateKey initialPrivateKey = EllipticalCurveKey.generatePrivateKey(ellipticalCurve, basePoint, n);
+            PublicKey publicKey = EllipticalCurveKey.generatePublicKey(ellipticalCurve, basePoint, n, initialPrivateKey);
+
+            DigitalSigning digitalSigning = new DigitalSigning(ellipticalCurve, basePoint, n, initialPrivateKey);
+            byte[] sign = digitalSigning.getSigning(hash);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        // TODO: Sign dan Public Key dimasukkan ke dalam email
 
         encryptionKey = findViewById(R.id.encrypt_key);
         encryptionKeyCheckBox = findViewById(R.id.encrypt_message);
